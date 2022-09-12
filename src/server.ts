249223103132df8e3b5aa537cf8610b4ab8643f0
Elona Kajtazi <MY_NAME@example.com>
2022-09-13 00:52:29 +0200

    GET (READING) for a single  user and hobby done!

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,33 @@ app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany({ include: { hobby: true } });
   res.send(users);
 });
+app.get("/users/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const user = await prisma.user.findUnique({
+    where: { id: id },
+    include: { hobby: true },
+  });
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send({ error: "User not found" });
+  }
+});
 app.get("/hobbies", async (req, res) => {
   const hobbies = await prisma.hobby.findMany({ include: { user: true } });
   res.send(hobbies);
+});
+app.get("/hobbies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const hobby = await prisma.hobby.findUnique({
+    where: { id },
+    include: { user: true },
+  });
+  if (hobby) {
+    res.send(hobby);
+  } else {
+    res.status(404).send({ error: "Hobby not found" });
+  }
 });
 
 app.listen(port, () => {
